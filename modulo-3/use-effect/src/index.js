fe9b94af89f21196b1feea662fb4ae024da0ec62
Lucas Macedo <lucasmacedo@msn.com>
2022-05-17 23:07:30 -0300

    exercício use-effect

---
+++ modulo-3/use-effect/src/index.js
@@ -0,0 +1,22 @@
+// import React from "react";
+// import ReactDOM from "react-dom";
+
+// import App from "./App";
+
+// const rootElement = document.getElementById("root");
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+//   rootElement
+// );
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
