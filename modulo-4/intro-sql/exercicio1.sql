7bad3a701b854463ab52dd5adf5e398f22d74533
Lucas Macedo <lucasmacedo@msn.com>
2022-07-11 17:02:08 -0300

    exercicios sql

---
+++ modulo-4/intro-sql/exercicio1.sql
@@ -0,0 +1,9 @@
+USE `aragon-lucas-rezende`;
+
+SET SQL_SAFE_UPDATES = 0;
+
+CREATE TABLE `Funcionários` (
+  id INT PRIMARY KEY,
+  name VARCHAR(255),
+  EMAIL vARCHAR(255)
+);
---
+++ modulo-4/intro-sql/exercicio4.sql
@@ -0,0 +1,7 @@
+INSERT INTO `Funcionários` 
+VALUES (004,"Yuzo","yuzo@lbn.com");
+
+DELETE FROM `Funcionários`
+WHERE id = 004;
+
+SELECT * FROM `Funcionários`;
